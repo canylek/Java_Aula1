7c3a5d78124eefd2ca9026fbcf0abed6b04f6d14
canylek <canylekbr@gmail.com>
2024-05-02 17:33:45 -0300

    arrumando exercicio

--- aulas/exercicio1/src/Main.java
+++ aulas/exercicio1/src/Main.java
@@ -6,22 +6,14 @@ public class Main {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-
-        double largura, comprimento, valor_m, area, preco;
-
-
         System.out.println("digite a largura do terreno em metros:");
-        largura = sc.nextDouble();
-        sc.nextLine();
+        double largura = sc.nextDouble();
         System.out.println("digite o comprimento do terreno em metros:");
-        comprimento = sc.nextDouble();
-        sc.nextLine();
+        double comprimento = sc.nextDouble();
         System.out.println("digite o valor do metro quadrado do terreno: (apenas numeros)");
-        valor_m = sc.nextDouble();
-        sc.nextLine();
-
-        area = largura * comprimento;
-        preco = area * valor_m;
+        double valor_m = sc.nextDouble();
+        double area = largura * comprimento;
+        double preco = area * valor_m;
 
         System.out.printf("A área do terreno é %.2f metros quadrados e o preço é R$ %.2f reais", area, preco);
         sc.close();
